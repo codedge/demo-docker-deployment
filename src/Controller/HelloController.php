6cd45dca2a2ea971c618bf27b5355e0e5d467872
Holger Lösken <holger.loesken@codedge.de>
2025-05-12 19:14:07 +0200

    Index route

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,17 +5,32 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HelloController extends AbstractController
 {
+    public function __construct(
+        #[Autowire(param: 'super_secret')]
+        private string $secret
+    ) {
+    }
+
+
+    #[Route('/')]
+    public function indexAction(): Response
+    {
+        return new Response('Startseite...');
+    }
+
     #[Route('json')]
     public function jsonOutput(): JsonResponse {
         $data = [
             'some_sample_text' => 'Hello Backend guys!',
+            'super_secret' => $this->secret,
         ];
-
 
         return $this->json($data);
     }
